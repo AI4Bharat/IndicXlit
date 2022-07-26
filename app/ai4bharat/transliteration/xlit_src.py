6c7c4c3d2e30635191960d577db9c603455d352b
Gokul NC <gokulnc@ymail.com>
2022-07-26 15:52:58 +0000

    [apps] bugfix

--- app/ai4bharat/transliteration/xlit_src.py
+++ app/ai4bharat/transliteration/xlit_src.py
@@ -1,13 +1,13 @@
 def XlitEngine(
     lang2use = "all", beam_width=4, rescore=True,
     model_type = "transformer",
-    src_script_type = "indic",
+    src_script_type = "roman",
 ):
     if model_type == "transformer":
-        if src_script_type == "indic":
+        if src_script_type in {"roman", "latin", "en"}:
             from .transformer import XlitEngineTransformer_En2Indic
             return XlitEngineTransformer_En2Indic(lang2use, beam_width=beam_width, rescore=rescore)
-        elif src_script_type in {"roman", "latin", "en"}:
+        elif src_script_type == "indic":
             from .transformer import XlitEngineTransformer_Indic2En
             return XlitEngineTransformer_Indic2En(beam_width=beam_width, rescore=rescore)
     
