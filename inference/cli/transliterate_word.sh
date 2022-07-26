2b62acd8efd0c213003dad0e628e48722366e069
yashmadhani97 <yashmadhani1997@gmail.com>
2022-07-27 01:41:25 +0530

    updatin command line option cli interface

--- inference/cli/transliterate_word.sh
+++ inference/cli/transliterate_word.sh
@@ -1,7 +1,12 @@
-lang_abr=$1
-input_file=$2
-beam=$3
-nbest=$4
-rerank=$5
+while getopts l:i:b:n:r: module
+do
+    case "${module}" in
+        l)lang_abr=${OPTARG};;
+        i)input_file=${OPTARG};;
+        b)beam=${OPTARG};;
+        n)nbest=${OPTARG};;
+        r)rerank=${OPTARG};;
+    esac
+done
 bash interactive.sh $lang_abr $input_file $beam $nbest 
 python3 generate_result_files.py $lang_abr $rerank
